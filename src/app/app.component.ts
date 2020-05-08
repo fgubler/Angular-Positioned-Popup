2fa3ced71af5a65dd5c1021ef40df5f9bc001347
Gubler, Florian <florian.gubler@ebp.ch>
2020-05-08 16:52:26 +0200

    use the example popup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {PopupService} from "./example-popup/popup.service";
+import {LatchOrientation} from "./positioned-popup/enums/latch-orientation";
+import {
+  createBoxClickEvent,
+  createPixelCoordinatesFromMouseEvent
+} from "./positioned-popup/helpers/click-event-creation";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'positioned-popup';
+  @ViewChild('topLeft', {static: true}) private topLeft: ElementRef<HTMLInputElement>;
+
+  readonly tooltip = 'Click to open popup!';
+
+  constructor(
+    private popupService: PopupService
+  ) {}
+
+  onClickTopLeft(event: MouseEvent) {
+    const clickedCoordinates = createPixelCoordinatesFromMouseEvent(event);
+    const boxClickEvent = createBoxClickEvent(clickedCoordinates, this.topLeft.nativeElement);
+    const popupData = {
+      clickEvent: boxClickEvent,
+      latchOrientation: LatchOrientation.horizontal,
+    };
+    this.popupService.openOverlay(popupData);
+  }
 }
